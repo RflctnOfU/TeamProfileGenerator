cc2c9e4937c9b374756461c1ebf06ce78bd78f9a
Kris Marshall <rflctnofu@hotmail.com>
2022-07-19 23:38:16 -0400

    removed path module from requirements

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // INDEX.JS //
 const inquirer = require('inquirer')
 const fs = require('fs')
-const path = require('path')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
