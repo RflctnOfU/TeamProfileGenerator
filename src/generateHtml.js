d460c2bdceb7924300943a9e3f94e47ca18ffa3e
Kris Marshall <rflctnofu@hotmail.com>
2022-07-20 18:32:39 -0400

    tried doing document.createElement, but error is thrown

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,3 +1,7 @@
+global.document = new JSDOM(html).window.document;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 function generateCards(teamInfo) {
     const roles = teamInfo.map(member => {
         return member.getRole();
@@ -23,28 +27,62 @@ function generateCards(teamInfo) {
 
     const getIcon = teamInfo.map(member => {
         if (member.getRole() === 'Manager') {
-            return '<i class="fa-solid fa-mug-hot fa-xl mr-3"></i>';
+            return `'fa-solid', 'fa-mug-hot', 'fa-xl', 'mr-3'`;
         } else if (member.getRole() === 'Engineer') {
-            return '<i class="fa-solid fa-glasses fa-xl mr-3"></i>';
+            return `'fa-solid', 'fa-glasses', 'fa-xl', 'mr-3'`;
         } else if (member.getRole() === 'Intern') {
-            return '<i class="fa-solid fa-user-graduate fa-xl mr-3"></i>';
+            return `'fa-solid', 'fa-user-graduate', 'fa-xl', 'mr-3'`;
         }
     })
 
     for (let i = 0; i < teamInfo.length; i++) {
-        `<div class="col-3">
-        <div class="card shadow">
-            <div class="card-body bg-primary text-white rounded-lg">
-              <h2 class="card-title">${name[i]}</h5>
-                ${getIcon[i]}<h4 class="card-text d-inline-flex">${roles[i]}</h4>
-            </div>
-            <ul class="list-group bg-light">
-              <li class="shadow-sm border list-group-item mt-4 mb-1 ml-4 mr-4">ID: ${id[i]} </li>
-              <li class="shadow-sm border list-group-item mt-1 mb-1 ml-4 mr-4">email: <a href="mailto:${email[i]}">${email[i]}</a></li>
-              <li class="shadow-sm border list-group-item mt-1 mb-4 ml-4 mr-4">${offGitSch[i]}</li>
-            </ul>
-          </div>
-    </div>`
+        //   return  `<div class="col-3">
+        //     <div class="card shadow">
+        //         <div class="card-body bg-primary text-white rounded-lg">
+        //           <h2 class="card-title">${name[i]}</h5>
+        //             ${getIcon[i]}<h4 class="card-text d-inline-flex">${roles[i]}</h4>
+        //         </div>
+        //         <ul class="list-group bg-light">
+        //           <li class="shadow-sm border list-group-item mt-4 mb-1 ml-4 mr-4">ID: ${id[i]} </li>
+        //           <li class="shadow-sm border list-group-item mt-1 mb-1 ml-4 mr-4">email: <a href="mailto:${email[i]}">${email[i]}</a></li>
+        //           <li class="shadow-sm border list-group-item mt-1 mb-4 ml-4 mr-4">${offGitSch[i]}</li>
+        //         </ul>
+        //       </div>
+        // </div>`
+        const cardDiv = document.createElement('div');
+        const card = document.createElement('div');
+        const cardBody = document.createElement('div');
+        const cardTitle = document.createElement('h2');
+        const cardIcon = document.createElement('i');
+        const cardType = document.createElement('h4');
+        const ul = document.createElement('ul');
+        const li1 = document.createElement('li');
+        const li2 = document.createElement('li');
+        // const aEmail = document.createElement('a');
+        const li3 = document.createElement('li');
+
+        cardDiv.classList.add('col-3');
+        card.classList.add('card', 'shadow');
+        cardBody.classList.add('card-body', 'bg-primary', 'text-white', 'rounded-lg');
+        cardTitle.classList.add('card-title');
+        cardIcon.classList.add(`${getIcon[i]}`);
+        cardType.classList.add('card-text', 'd-inline-flex');
+        ul.classList.add('list-group', 'bg-light');
+        li1.classList.add('shadow-sm', 'border', 'list-group-item', 'mt-4', 'mb-1', 'ml-4', 'mr-4');
+        li2.classList.add('shadow-sm', 'border', 'list-group-item', 'mt-1', 'mb-1', 'ml-4', 'mr-4');
+        li3.classList.add('shadow-sm', 'border', 'list-group-item', 'mt-1', 'mb-4', 'ml-4', 'mr-4');
+
+        cardTitle.innerText = name[i];
+        cardType.innerText = roles[i];
+        li1.innerText = `ID: ${id[i]}`
+        li2.innerText = `email: <a href="mailto: ${email[i]}">${email[i]}</a>`
+        li3.innerText = offGitSch[i];
+
+        ul.append(li1, li2, li3);
+        cardBody.append(cardTitle, cardIcon, cardType);
+        card.append(cardBody, ul);
+        cardDiv.append(card);
+
     }
 
 }
