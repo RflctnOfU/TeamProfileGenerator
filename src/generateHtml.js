8b554cc5472de05a84ed6c2d447856a44e1e546d
Kris Marshall <rflctnofu@hotmail.com>
2022-07-20 21:15:11 -0400

    still trying

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,89 +1,108 @@
-global.document = new JSDOM(html).window.document;
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
 function generateCards(teamInfo) {
-    const roles = teamInfo.map(member => {
-        return member.getRole();
-    })
-    const name = teamInfo.map(member => {
-        return member.name;
-    })
-    const id = teamInfo.map(member => {
-        return member.id;
-    })
-    const email = teamInfo.map(member => {
-        return member.email;
-    })
-    const offGitSch = teamInfo.map(member => {
-        if (member.getRole() === 'Manager') {
-            return `Office Number: ${member.getOffice()}`
-        } else if (member.getRole() === 'Engineer') {
-            return `GitHub: <a href="https://github.com/${member.getGithub()} target="_blank">${member.getGithub()}</a>`
-        } else if (member.getRole() === 'Intern') {
-            return `School: ${member.getSchool()}`
-        }
-    })
+    // const arr = [];
 
-    const getIcon = teamInfo.map(member => {
-        if (member.getRole() === 'Manager') {
-            return `'fa-solid', 'fa-mug-hot', 'fa-xl', 'mr-3'`;
-        } else if (member.getRole() === 'Engineer') {
-            return `'fa-solid', 'fa-glasses', 'fa-xl', 'mr-3'`;
-        } else if (member.getRole() === 'Intern') {
-            return `'fa-solid', 'fa-user-graduate', 'fa-xl', 'mr-3'`;
-        }
-    })
+    // const roles = teamInfo.map(member => {
+    //     arr.push(member.getRole());
 
-    for (let i = 0; i < teamInfo.length; i++) {
-        //   return  `<div class="col-3">
-        //     <div class="card shadow">
-        //         <div class="card-body bg-primary text-white rounded-lg">
-        //           <h2 class="card-title">${name[i]}</h5>
-        //             ${getIcon[i]}<h4 class="card-text d-inline-flex">${roles[i]}</h4>
-        //         </div>
-        //         <ul class="list-group bg-light">
-        //           <li class="shadow-sm border list-group-item mt-4 mb-1 ml-4 mr-4">ID: ${id[i]} </li>
-        //           <li class="shadow-sm border list-group-item mt-1 mb-1 ml-4 mr-4">email: <a href="mailto:${email[i]}">${email[i]}</a></li>
-        //           <li class="shadow-sm border list-group-item mt-1 mb-4 ml-4 mr-4">${offGitSch[i]}</li>
-        //         </ul>
-        //       </div>
-        // </div>`
-        const cardDiv = document.createElement('div');
-        const card = document.createElement('div');
-        const cardBody = document.createElement('div');
-        const cardTitle = document.createElement('h2');
-        const cardIcon = document.createElement('i');
-        const cardType = document.createElement('h4');
-        const ul = document.createElement('ul');
-        const li1 = document.createElement('li');
-        const li2 = document.createElement('li');
-        // const aEmail = document.createElement('a');
-        const li3 = document.createElement('li');
-
-        cardDiv.classList.add('col-3');
-        card.classList.add('card', 'shadow');
-        cardBody.classList.add('card-body', 'bg-primary', 'text-white', 'rounded-lg');
-        cardTitle.classList.add('card-title');
-        cardIcon.classList.add(`${getIcon[i]}`);
-        cardType.classList.add('card-text', 'd-inline-flex');
-        ul.classList.add('list-group', 'bg-light');
-        li1.classList.add('shadow-sm', 'border', 'list-group-item', 'mt-4', 'mb-1', 'ml-4', 'mr-4');
-        li2.classList.add('shadow-sm', 'border', 'list-group-item', 'mt-1', 'mb-1', 'ml-4', 'mr-4');
-        li3.classList.add('shadow-sm', 'border', 'list-group-item', 'mt-1', 'mb-4', 'ml-4', 'mr-4');
-
-        cardTitle.innerText = name[i];
-        cardType.innerText = roles[i];
-        li1.innerText = `ID: ${id[i]}`
-        li2.innerText = `email: <a href="mailto: ${email[i]}">${email[i]}</a>`
-        li3.innerText = offGitSch[i];
-
-        ul.append(li1, li2, li3);
-        cardBody.append(cardTitle, cardIcon, cardType);
-        card.append(cardBody, ul);
-        cardDiv.append(card);
-
+    // })
+    // const name = teamInfo.map(member => {
+    //     arr.push(member.getName());
+    // })
+    // const id = teamInfo.map(member => {
+    //     arr.push(member.getId());
+    // })
+    // const email = teamInfo.map(member => {
+    //     arr.push(member.getEmail());
+    // })
+    // const offGitSch = teamInfo.map(member => {
+    //     if (member.getRole() === 'Manager') {
+    //         // return `Office Number: ${member.getOffice()}`
+    //         arr.push(getOffice());
+    //     } else if (member.getRole() === 'Engineer') {
+    //         arr.push(member.getGithub());
+    //         // return `GitHub: <a href="https://github.com/${member.getGithub()} target="_blank">${member.getGithub()}</a>`
+    //     } else if (member.getRole() === 'Intern') {
+    //         arr.push(member.getSchool());
+    //         // return `School: ${member.getSchool()}`
+    //     }
+    // })
+    // console.log(teamInfo);
+    const createManager = (manager) => {
+        return `<div class="col-3">
+            <div class="card shadow">
+                <div class="card-body bg-primary text-white rounded-lg">
+                  <h2 class="card-title">${manager.getName()}</h5>
+                  <i class="fa-solid fa-mug-hot fa-xl mr-3"></i><h4 class="card-text d-inline-flex">${manager.getRole()}</h4>
+                </div>
+                <ul class="list-group bg-light">
+                  <li class="shadow-sm border list-group-item mt-4 mb-1 ml-4 mr-4">ID: ${manager.getId()} </li>
+                  <li class="shadow-sm border list-group-item mt-1 mb-1 ml-4 mr-4">email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                  <li class="shadow-sm border list-group-item mt-1 mb-4 ml-4 mr-4">Office Number: ${manager.getOffice()}</li>
+                </ul>
+              </div>
+        </div>`;
     }
+    const createEngineer = (engineer) => {
+        return `<div class="col-3">
+            <div class="card shadow">
+                <div class="card-body bg-primary text-white rounded-lg">
+                  <h2 class="card-title">${engineer.getName()}</h5>
+                  <i class="fa-solid fa-glasses fa-xl mr-3"></i><h4 class="card-text d-inline-flex">${engineer.getRole()}</h4>
+                </div>
+                <ul class="list-group bg-light">
+                  <li class="shadow-sm border list-group-item mt-4 mb-1 ml-4 mr-4">ID: ${engineer.getId()} </li>
+                  <li class="shadow-sm border list-group-item mt-1 mb-1 ml-4 mr-4">email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                  <li class="shadow-sm border list-group-item mt-1 mb-4 ml-4 mr-4">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+                </ul>
+              </div>
+        </div>`;
+    }
+    const createIntern = (intern) => {
+        return `<div class="col-3">
+            <div class="card shadow">
+                <div class="card-body bg-primary text-white rounded-lg">
+                  <h2 class="card-title">${intern.getName()}</h5>
+                  <i class="fa-solid fa-user-graduate fa-xl mr-3"></i><h4 class="card-text d-inline-flex">${intern.getRole()}</h4>
+                </div>
+                <ul class="list-group bg-light">
+                  <li class="shadow-sm border list-group-item mt-4 mb-1 ml-4 mr-4">ID: ${intern.getId()} </li>
+                  <li class="shadow-sm border list-group-item mt-1 mb-1 ml-4 mr-4">email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                  <li class="shadow-sm border list-group-item mt-1 mb-4 ml-4 mr-4">School: ${intern.getSchool()}</li>
+                </ul>
+              </div>
+        </div>`;
+    }
+
+    for (const element of teamInfo) {
+        if (element.getRole() === 'Manager') {
+            console.log('Manager');
+            createManager(element);
+        } else if (element.getRole() === 'Engineer') {
+            console.log('Engineer');
+            createEngineer(element);
+        } else if (element.getRole() === 'Intern') {
+            console.log('Intern');
+            createIntern(element);
+        }
+    }
+    // for (let i = 0; i < teamInfo.length; i++) {
+    //     if (teamInfo.map(member => {
+    //         member.getRole()
+    //     }) === 'Manager') {
+    //         console.log("Manager");
+    //         // createManager(teamInfo);
+    //     } else if (teamInfo.map(member => {
+    //         member.getRole()
+    //     }) === 'Engineer') {
+    //         console.log("Engineer");
+    //         // createEngineer(teamInfo);
+    //     } else if (teamInfo.map(member => {
+    //         member.getRole()
+    //     }) === 'Intern') {
+    //         console.log("Intern");
+    //         // createIntern(teamInfo);
+    //     }
+    // }
 
 }
 
